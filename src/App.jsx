dc2a60a9d998bd34d9217d5104ae5f72feadf532
Sombhu2022 <sombhudas93@gmail.com>
2024-08-14 16:54:36 +0530

    setup react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='text-3xl border rounded bg-red-500'> app page</div>
+    </>
+  )
+}
+
+export default App
